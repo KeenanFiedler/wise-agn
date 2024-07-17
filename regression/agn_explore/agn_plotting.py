8f9c0d3255bf861898d2b1c67667b5200169cd22
KeenanFiedler <keenan.fiedler@gmail.com>
2024-07-17 13:29:29 -0400

    Updated sdss and desi cuts/fits

--- regression/agn_explore/agn_plotting.py
+++ regression/agn_explore/agn_plotting.py
@@ -6,28 +6,29 @@ import matplotlib as mpl
 import matplotlib.mlab as mlab
 
 from scipy.stats import norm
+from scipy.stats import skewnorm
 
 def make_redshift_plot(data, survey):
     z = np.array(data.z)
     zerr = np.array(data.zerr)
-    ind = np.where((zerr<0.005) & (zerr>0))[0]
+    ind = np.where((zerr<0.005) & (zerr>0) & (z>0.1))[0]
     z=z[ind]
     zerr=zerr[ind]
 
-    plt.scatter(z, zerr, s=0.5, linewidths=0)
+    plt.scatter(z, zerr/z, s=0.5, linewidths=0)
     plt.xlabel('Redshift')
-    plt.ylabel('Redshift Error')
+    plt.ylabel('Redshift Error/Redshift')
     plt.title('Redshift vs Error')
     plt.savefig(survey + '_redshift.png', dpi=300)
     plt.clf()
 
 def make_histogram(w1, z):
-    (w1_mu, w1_sig) = norm.fit(w1)
+    (w1_mu, w1_sig,w1_skew) = skewnorm.fit(w1)
     (z_mu, z_sig) = norm.fit(z)
 
     _, bins, _ = plt.hist(w1, bins=40, density=True)
 
-    w1_norm = norm.pdf(bins, w1_mu, w1_sig)
+    w1_norm = skewnorm.pdf(bins, w1_mu, w1_sig, w1_skew)
     plt.plot(bins, w1_norm, 'r--', linewidth=2)
 
     plt.xlabel('W1 Magnitude')
@@ -56,21 +57,13 @@ def main():
     desi = pd.read_csv('desi.csv')
     sdss = pd.read_csv('sdss.csv')
 
-    ind_sdss = sdss.z<1
-    ind_desi = desi.z<1
-    sdss = sdss[ind_sdss]
-    desi = desi[ind_desi]
-    ind_sdss = sdss.zerr<0.005
-    ind_desi = desi.zerr<0.005
-    sdss = sdss[ind_sdss]
-    desi = desi[ind_desi]
     z = np.array(sdss.z)
     z = np.append(z, desi.z)
     w1 = np.array(sdss.t1_w1)
     w1 = np.append(w1, desi.t1_w1)
 
 
-    #make_redshift_plot(sdss, 'sdss')
+    make_redshift_plot(desi, 'desi')
     make_histogram(w1, z)
     draw_normal(z,1)
 
